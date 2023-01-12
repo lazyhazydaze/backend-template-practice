fa9e5b5cdee7d673665638e508fae75a997e614d
lazyhazydaze <juruissic@gmail.com>
2023-01-12 23:14:06 +0800

    update main index.js file to add in db, routers, controllers, and main route

--- backend_template/index.js
+++ backend_template/index.js
@@ -3,12 +3,23 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const db = require("./db/models/index");
+const { product } = db;
+
+const ProductsRouter = require("./routers/ProductsRouter");
+const ProductsController = require("./controllers/ProductsController");
+
 const PORT = process.env.PORT || 8080;
 const app = express();
+
+const productsController = new ProductsController(product);
+const productsRouter = new ProductsRouter(express, productsController).routes();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use("/products", productsRouter);
 
 app.listen(PORT, () => {
   console.log(`Application listening to port ${PORT} yey`);
